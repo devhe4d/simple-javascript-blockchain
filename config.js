c28eff8c6be866cdae1a37b14eea8a7b682224d2
Shahryar <shahryar.seven@gmail.com>
2021-04-01 21:47:12 +0300

    elliptic added to wallet for public/private keys

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
 const INITIAL_DIFFICULTY = 3;
 const MINE_RATE = 1000;
+const STARTING_BALANCE = 1000;
+
 const GENESIS_DATA = {
   timestamp: 1,
   lastHash: 'last-hash-xx0',
@@ -9,4 +11,4 @@ const GENESIS_DATA = {
   data: [],
 };
 
-module.exports = { GENESIS_DATA, MINE_RATE };
+module.exports = { GENESIS_DATA, MINE_RATE, STARTING_BALANCE };
