9517193e0883a4d71b9554ae7c9829a8d1ac4c9b
Shahryar <shahryar.seven@gmail.com>
2021-04-01 20:12:14 +0300

    pubsub channels configed for nodes to talk to each other

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const Blockchain = require('./blockchain');
-
+const PubSub = require('./pubsub');
+const request = require('request');
 const app = express();
 const blockchain = new Blockchain();
+const pubsub = new PubSub({ blockchain });
 
+const DEFAULT_PORT = 3000;
+const ROOT_NODE_ADDRESS = `hhtp://localhost:${DEFAULT_PORT}`;
+
+setTimeout(() => pubsub.broadcastChain(), 1000);
 app.use(express.json());
 
 app.get('/api/blocks', (req, res) => {
@@ -15,9 +21,32 @@ app.post('/api/mine', (req, res) => {
 
   blockchain.addBlock({ data });
 
+  pubsub.broadcastChain();
+
   res.redirect('/api/blocks');
 });
 
-app.listen(3000, () => {
-  console.log('App listening on port 3000!');
+function syncChains() {
+  request({ url: `${ROOT_NODE_ADDRESS}/api/blocks` }, (err, res, body) => {
+    if (!err && res.statusCode === 200) {
+      const rootChain = JSON.parse(body);
+      blockchain.replaceChain(rootChain);
+
+      console.log('replace chain on a sync with', rootChain);
+    }
+  });
+}
+
+let PEER_PORT;
+
+if (process.env.GENERATE_PEER_PORT === 'true') {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+const PORT = PEER_PORT || DEFAULT_PORT;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+
+  if (PORT !== DEFAULT_PORT) {
+    syncChains();
+  }
 });
